f3dc349807d27ba5e5cd12d2577ebf0240546f6c
Michał Zajączkowski <79537829+MichalZajaczkowski@users.noreply.github.com>
2021-12-27 21:30:13 +0100

    Initial commit
    Initial commit

---
+++ src/test/java/EventsProject/EventsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package EventsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
