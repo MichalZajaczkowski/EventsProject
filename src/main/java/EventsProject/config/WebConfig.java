cfd9c9c62d1ee6bd367c1feb9d40029f1f6e267d
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2021-12-30 18:55:11 +0100

    Started adding elements for locale feature. Added WebMVC to configure locales. Added navbar with languages options

---
+++ src/main/java/EventsProject/config/WebConfig.java
@@ -0,0 +1,33 @@
+package EventsProject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("pl"));
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor LocaleChangeInterceptor () {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(LocaleChangeInterceptor());
+    }
+}
