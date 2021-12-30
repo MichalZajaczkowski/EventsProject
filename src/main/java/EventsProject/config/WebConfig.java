3bbc75edc76084235d1c7aaef5594425dd1d9d77
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2021-12-30 20:17:53 +0100

    Change webConfig - localization change works now. Deleted useless interface

--- src/main/java/EventsProject/config/WebConfig.java
+++ src/main/java/EventsProject/config/WebConfig.java
@@ -1,6 +1,7 @@
 package EventsProject.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -9,13 +10,15 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
+@ComponentScan
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-        localeResolver.setDefaultLocale(new Locale("pl"));
+        Locale polishLocale = new Locale("pl");
+        localeResolver.setDefaultLocale(polishLocale);
         return localeResolver;
     }
 
