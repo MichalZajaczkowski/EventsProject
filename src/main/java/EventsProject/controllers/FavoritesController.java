ed04c7acf06fe65aef9dd9f12c07bb7ee91b2f68
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2021-12-30 18:09:41 +0100

    Fixed structure problems, added controllers and html's for subpages

--- src/main/java/EventsProject/controllers/FavoritesController.java
+++ src/main/java/EventsProject/controllers/FavoritesController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
-public class mainController {
+public class FavoritesController implements ControllerEntity{
 
-    @GetMapping("/")
-    public String displayMainPackage() {
-        return "main";
+    @GetMapping("favorites")
+    public String displayPage() {
+        return "favorites/index";
     }
 }
---
+++ src/main/java/EventsProject/controllers/MainController.java
@@ -0,0 +1,15 @@
+package EventsProject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class MainController implements ControllerEntity{
+
+    @GetMapping("/")
+    public String displayPage() {
+        return "index";
+    }
+}
